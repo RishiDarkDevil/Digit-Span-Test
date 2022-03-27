4acbae35acf9326a44906abe9c491df49ab9c7c1
RishiDarkDevil <rishi8001100192@gmail.com>
2022-03-27 23:17:31 +0530

    Add Digit Pad Buttons

--- app.R
+++ app.R
@@ -58,22 +58,31 @@ DigitPadUI <- fluidPage(
       actionButton("9", "9", style='padding-left:25px; padding-right:25px; font-size:700%; margin: 25px')
     )
   ),
+  fluidRow(
+    column(12, align = "center",
+           actionButton("backspace", "", icon = icon("backspace"), style='padding-left:25px; padding-right:25px; font-size:700%; margin: 25px'),
+           actionButton("0", "0", style='padding-left:25px; padding-right:25px; font-size:700%; margin: 25px'),
+           actionButton("next_correct", "", icon = icon("arrow-right"), style='padding-left:25px; padding-right:25px; font-size:700%; margin: 25px')
+    )
+  )
 )
 
 #--------------------------- Digit Pad Disable
 disable_DigitPad <- function() {
-  disable("1");disable("2");disable("3");disable("4");disable("5");disable("6");disable("7");disable("8");disable("9");
+  disable("1");disable("2");disable("3");disable("4");disable("5");disable("6");disable("7");disable("8");disable("9");disable("0");disable("backspace");disable("next_correct")
   updateActionButton(inputId = "1", label = "?");updateActionButton(inputId = "2", label = "?");updateActionButton(inputId = "3", label = "?");
   updateActionButton(inputId = "4", label = "?");updateActionButton(inputId = "5", label = "?");updateActionButton(inputId = "6", label = "?");
-  updateActionButton(inputId = "7", label = "?");updateActionButton(inputId = "8", label = "?");updateActionButton(inputId = "9", label = "?")
+  updateActionButton(inputId = "7", label = "?");updateActionButton(inputId = "8", label = "?");updateActionButton(inputId = "9", label = "?");
+  updateActionButton(inputId = "0", label = "?");updateActionButton(inputId = "backspace", icon = icon("question"));updateActionButton(inputId = "next_correct", icon = icon("question"))
 }
 
 #--------------------------- Digit Pad Enable
 enable_DigitPad <- function() {
-  enable("1");enable("2");enable("3");enable("4");enable("5");enable("6");enable("7");enable("8");enable("9");
+  enable("1");enable("2");enable("3");enable("4");enable("5");enable("6");enable("7");enable("8");enable("9");enable("0");enable("backspace");enable("next_correct")
   updateActionButton(inputId = "1", label = "1");updateActionButton(inputId = "2", label = "2");updateActionButton(inputId = "3", label = "3");
   updateActionButton(inputId = "4", label = "4");updateActionButton(inputId = "5", label = "5");updateActionButton(inputId = "6", label = "6");
   updateActionButton(inputId = "7", label = "7");updateActionButton(inputId = "8", label = "8");updateActionButton(inputId = "9", label = "9");
+  updateActionButton(inputId = "0", label = "0");updateActionButton(inputId = "backspace", label = "", icon = icon("backspace"));updateActionButton(inputId = "next_correct", label = "", icon = icon("arrow-right"));
 }
 
 # ------------------------- Test UI
@@ -130,12 +139,13 @@ server <- function(input, output, session) {
   #------------- Deals with Conducting Test
   observeEvent(input$start, {
     active(1)
-    dig_seq(sample(1:10, 10, replace = FALSE))
+    dig_seq(sample(0:9, no_of_digs(), replace = FALSE))
   })
   
-  disp_dig <- reactiveVal(0)
+  disp_dig <- reactiveVal(-1)
   active <- reactiveVal(11)
-  dig_seq <- reactiveVal(sample(1:10, 10, replace = FALSE))
+  dig_seq <- reactiveVal(sample(0:9, 2, replace = FALSE))
+  no_of_digs <- reactiveVal(2)
   
   output$display_digit <- renderText({
     disp_dig()
@@ -144,7 +154,7 @@ server <- function(input, output, session) {
   observe({
     invalidateLater(1000, session)
     isolate({
-      if (active() <= 10) {
+      if (active() <= no_of_digs()) {
         disp_dig(dig_seq()[active()])
         active(active() + 1)
         disable_DigitPad()
@@ -152,6 +162,13 @@ server <- function(input, output, session) {
         enable_DigitPad()
       }
     })
+  })
+  
+  observeEvent(input$next_correct, {
+    no_of_digs(no_of_digs() + 1)
+    disp_dig(-1)
+    active(1)
+    dig_seq(sample(0:9, no_of_digs(), replace = FALSE))
   })
 }
 
