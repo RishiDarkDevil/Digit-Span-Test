a86a06be8b894ed6a6f08063f9f68f33b0c7564e
RishiDarkDevil <rishi8001100192@gmail.com>
2022-03-28 17:16:27 +0530

    Add Correct Digit Input Check Digit Pad

--- helper.R
+++ helper.R
@@ -1,12 +1,1 @@
-educat_choices <- c(0, 1, 2, 3, 4)
-educat_choices <- setNames(educat_choices, c("Still in school, below 12th Grade", "12th Grade", "Bachelors", "Masters", "PhD"))
-educat_choices
-
-job_choices <- c(0, 1, 2, 3)
-job_choices <- setNames(job_choices, c("Academia", "Industry", "Business", "Not working yet"))
-job_choices
-
-env_choices <- c(0, 1, 2, 3)
-env_choices <- setNames(env_choices, c("Silent", "Normal", "Little Noisy", "Very Noisy"))
-env_choices
 
